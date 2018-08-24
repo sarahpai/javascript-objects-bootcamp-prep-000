ac60892e07a94a0abb48dc026ceb8908173d3028
Sarah Pai <pai.sarah89@gmail.com>
2018-08-24 21:42:04 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -17,3 +17,15 @@ return Object.assign({}, {[playlist]: artistName})
 //--------------------------- below is 1st attempt
 
 
+var playlist = {};
+
+var playlist = { 'artistName': 'songTitle' };
+
+function updatePlaylist (playlist, artistName, songTitle) {
+  return Object.assign(playlist, { 'Phil Ochs': "Here's to the State of Mississipi"})
+}
+
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist['Slowdive'];
+}
