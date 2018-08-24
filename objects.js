c3909e723d09e43f1f4f70c01050ec32a3684ee7
Sarah Pai <pai.sarah89@gmail.com>
2018-08-24 21:12:28 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,7 +2,7 @@ var playlist = {};
 var playlist = {'artist': 'song'};
 
 function updatePlaylist(playlist, artistName, songTitle) {
- playlist.artistName ['Phil Ochs': "Here's to the State of Mississipi"]
+updatePlaylist.artistName ['Phil Ochs': "Here's to the State of Mississipi"]
  return updatePlaylist
 }
 
