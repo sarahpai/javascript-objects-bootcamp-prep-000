dfc64ef87ef53938e7b0096a3c6faa266e249fa3
Sarah Pai <pai.sarah89@gmail.com>
2018-08-24 21:13:49 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,7 +2,5 @@ var playlist = {};
 var playlist = {'artist': 'song'};
 
 function updatePlaylist(playlist, artistName, songTitle) {
-updatePlaylist.artistName ['Phil Ochs': "Here's to the State of Mississipi"]
- return updatePlaylist
+return Object.assign(playlist, {['Phil Ochs']: "Here's to the State of Mississipi"})
 }
-
