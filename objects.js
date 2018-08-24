6e441f115825cbafaa0cd32951a960874a277115
Sarah Pai <pai.sarah89@gmail.com>
2018-08-24 21:41:45 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -9,25 +9,11 @@ return Object.assign(playlist, {['Phil Ochs']: "Here's to the State of Mississip
 function removeFromPlaylist(playlist, artistName){
   delete playlist['Slowdive'];
 return Object.assign({}, {[playlist]: artistName})
-  const playlist2 = {'Slowdive': 'Alison'}
-return playlist2
+ // const playlist2 = {'Slowdive': 'Alison'}
+//return playlist2
 }
 
 
 //--------------------------- below is 1st attempt
-
-
-var playlist = {};
-
-var playlist = { 'artistName': 'songTitle' };
-
-function updatePlaylist (playlist, artistName, songTitle) {
-  return Object.assign(playlist, { 'Phil Ochs': "Here's to the State of Mississipi"})
-}
-
-
-function removeFromPlaylist(playlist, artistName) {
-  delete playlist['Slowdive'];
-}
 
 
